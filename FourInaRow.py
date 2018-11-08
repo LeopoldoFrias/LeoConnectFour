7015fe4375b404caa9e6ccea965b3350f0835393
LeopoldoFrias <44326230+LeopoldoFrias@users.noreply.github.com>
2018-11-07 18:57:13 -0800

    Add files via upload

--- FourInaRow.py
+++ FourInaRow.py
@@ -1,7 +1,7 @@
-import sys, pygame, numpy
+import sys, pygame, numpy, math, random
 from pygame.locals import *
 
-# check math
+pygame.init()
 width = 700
 height = 700
 
@@ -21,15 +21,15 @@ WIDTH = 800
 HEIGHT = 800
 turn = 0
 #Global variables define what something means
-def Music():
-    pygame.mixer.pre_init()
-    pygame.mixer.init()
-    pygame.mixer.music.load('bensound-funkyelement.mp3')
-    pygame.mixer.volume_set(0.5)
-    pygame.mixer.music.play(-1)
+
+pygame.mixer.pre_init()
+pygame.mixer.init()
+pygame.mixer.music.load('bensound-funkyelement.mp3')
+#pygame.mixer.volume_set(3)
+pygame.mixer.music.play(-1)
 
 def createboard():
-    board = numpy.zeros((COLUMN_COUNT, ROW_COUNT)) # (ROW_COUNT, ROW_COUNT)
+    board = numpy.zeros((ROW_COUNT, COLUMN_COUNT)) # (ROW_COUNT, ROW_COUNT)
     return board
 
 def dropPiece(board, row, col, piece):
@@ -37,13 +37,12 @@ def dropPiece(board, row, col, piece):
 
 def validLocation(board, col):
     return board[ROW_COUNT-1][col] == 0
-# check here
+
 
 def getNextOpenRow(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
-# check here
 
 def printBoard(board):
     print(numpy.flip(board, 0))
@@ -59,13 +58,13 @@ def winningMove(board, piece):
             # all winning moves for vertical
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r+3][c] == piece:
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
                 return True
 
             # all winning moves for positive slope diagonals
     for c in range(COLUMN_COUNT - 3):
          for r in range(ROW_COUNT - 3):
-             if board[r][c] == piece and board[r + 1][c+1] == piece and board[r + 2][c+2] == piece and board[r + 3][c+2] == piece:
+             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+2] == piece:
                 return True
 
             # all winning moves for negative slope diagonals
@@ -89,13 +88,13 @@ def drawBoard(board):
     pygame.display.update()
 
 
+
 board = createboard()
 printBoard(board)
 drawBoard(board)
 pygame.font.init()
 myFont = pygame.font.SysFont("monospace", 75)
 game_over = False
-pygame.init()
 width = COLUMN_COUNT + SquareSize
 height = (ROW_COUNT+1) + SquareSize
 size = (width, height)
@@ -110,19 +109,19 @@ while not game_over:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SquareSize))
-            xpos = event.pos[0]
+            posx = event.pos[0]
             if turn == 0:
-                pygame.draw.circle(screen, RED, (xpos, int(SquareSize/2)), RADIUS)
+                pygame.draw.circle(screen, RED, (posx, int(SquareSize/2)), RADIUS)
             else:
-                pygame.draw.circle(screen, YELLOW, (xpos, int(SquareSize / 2)), RADIUS)
+                pygame.draw.circle(screen, YELLOW, (posx, int(SquareSize/2)), RADIUS)
         pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SquareSize))
-            #print(event.pos)
+            posx = event.position[0]
+            print(event.pos)
             if turn == 0:
-                xpos = event.position[0]
-                col = (int(xpos/SquareSize))
+                col = int(math.floor(posx/SquareSize))
 
                 if validLocation(board, col):
                     row = getNextOpenRow(board, col)
@@ -134,8 +133,8 @@ while not game_over:
                         game_over = True
 
             else:
-                xpos = event.position[0]
-                col = (int(xpos/SquareSize))
+                posx = event.position[0]
+                col = int(math.floor(posx/SquareSize))
 
                 if validLocation(board, col):
                     row = getNextOpenRow(board, col)
@@ -150,6 +149,7 @@ while not game_over:
                 pygame.mixer.music.load('bensound-clapandyell.mp3')
                 pygame.mixer.volume_set(0.5)
                 pygame.mixer.music.play(-1)
+                pygame.image.load('confetti.png')
                 # load up confetti sprite and make it fall down the screen
 
             printBoard(board)
